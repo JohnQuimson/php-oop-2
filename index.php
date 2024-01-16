d66203fad91e4206c8f6d2284e9d81e77b04178d
JohnQuimson <john.quimson777@gmail.com>
2024-01-16 17:44:20 +0100

    feat: creazione class animal

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 <?php
+
+class Animal
+{
+  public $foods;
+  public $toys;
+  public $kennels;
+
+  function __construct($_foods, $_toys, $_kennels)
+  {
+    $this->foods = $_foods;
+    $this->toys = $_toys;
+    $this->kennels = $_kennels;
+  }
+}
+
+$dog = new Animal(
+  ['Maiale', 'Manzo'],
+  ['Peluche', 'Palla', 'Osso', 'Corda', 'Frisbee'],
+  ['Brandina', 'Ciambella Pelosa', 'Cuscino', 'Coperta']
+);
+var_dump($dog);
+
+$cat = new Animal(
+  ['Tacchino', 'Pollo', 'Salmone'],
+  ['Topolino', 'Pallina', 'bacchetta', 'Laser'],
+  ['Amaca', 'Cuscino', 'Coperta']
+);
+var_dump($cat);
