9d2688199b119006d546c268b6c0869212f37f31
JohnQuimson <john.quimson777@gmail.com>
2024-01-17 23:11:44 +0100

    feat: tentativo exception

--- traits/Id.php
+++ traits/Id.php
@@ -4,15 +4,36 @@ trait Id
 {
   private $id;
 
-  // Getter
+  // Getter per l'ID
   public function get_id()
   {
     return $this->id;
   }
 
-  // Setter
+  // Setter per l'ID
   public function set_id($_id)
   {
     $this->id = $_id;
   }
 }
+
+// Tentativo di utilizzo di exception
+// trait Id
+// {
+//   private $id;
+
+//   // Getter
+//   public function get_id()
+//   {
+//     if (is_int($id)) {
+
+//       return $this->id;
+//     }
+//   }
+
+//   // Setter
+//   public function set_id($_id)
+//   {
+//     $this->id = $_id;
+//   }
+// }
